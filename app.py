efa97802493e61c645d2ebe3fa1fc197280f5bd5
Tasneem-I <107906533+Tasneem-I@users.noreply.github.com>
2023-04-29 16:25:35 +0530

    updates on backend
    Basic signup, login page created
    Backend api calls integrated

--- app.py
+++ app.py
@@ -1,31 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user
+from flask_session import Session
 import requests
 import json
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] ="AtigyiUS9812892019IKOSNJSGDU"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-#db = SQLAlchemy()
-#login_manager = LoginManager()
-#login_manager.init_app(app)
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 #database
-#class Users(UserMixin, db.Model):
-   #id = db.Column(db.Integer, primary_key = True)
+class Users(UserMixin, db.Model):
+   id = db.Column(db.Integer, primary_key = True)
+   firstname = db.Column(db.String(40), nullable = False)
+   lastname = db.Column(db.String(40), nullable=False)
+   email = db.Column(db.String(50), unique = True, nullable = False)
+   password = db.Column(db.String, nullable= False)
+   username = db.Column(db.String(40), nullable = False)
+   hash = db.Column(db.String(40), nullable = False)
+   sppassword = db.Column(db.String, nullable= False)
 
 
 
 
-#db.init_app(app)
-#with app.app_context:
-   #db.create_all()
 
-#@login_manager.user_loader
-#def user_load(user_id):
-   #return Users.query.get(user_id)
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+@login_manager.user_loader
+def user_load(user_id):
+   return Users.query.get(user_id)
 
 api_key = 'b96022655ebe44e0a332169e33124d1f'
 
@@ -62,12 +73,64 @@ def recipe_names():
         return render_template("recipes.html")
     
 
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        firstname = request.form.get("firstname")
+        lastname = request.form.get("lastname")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        call = "https://api.spoonacular.com/users/connect?apiKey=" + api_key
+        post = json.dumps({
+            'firstName' : firstname,
+            'lastName' : lastname,
+            'email' : email
+        })
+        header = {'Content-Type':'application/json'}
+        response = requests.request(method="POST",url=call,headers = header, data=post)
+        result = json.loads(response.text)
+        username = result["username"]
+        hash = result["hash"]
+        sppassword = result["spoonacularPassword"]
+        user = Users(firstname=firstname, lastname=lastname, email=email, password=password, username=username, hash=hash,sppassword=sppassword)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("login"))
+    else:   
+        return render_template("signup.html")
+    
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = Users.query.filter_by(email = request.form.get("email")).first()
+        if user.password == request.form.get("password"):
+            login_user(user)
+            session["user"] = user
+            return redirect(url_for("mealplan"))
+    return render_template("login.html")
+
 
 @app.route("/mealplan", methods=["GET", "POST"])
 def mealplan():
     if request.method == "POST":
         gen = 'https://api.spoonacular.com/mealplanner/generate'
+        user = session.get('user')
+        params ={"apiKey": api_key, "timeFrame": 'week'}
+        ans = requests.get(gen, params=params)
+        plan = json.loads(ans.text)
+        meal_names = []
+        for days in mealplan["week"]:
+            for i in range(0,3):
+                food =mealplan["week"][days]["meals"][i]["title"]
+                meal_names.append(food)
+        return redirect(url_for("mealplan", meals = meal_names))
+    else:
+        return render_template("mealplan.html")
 
+
+        
+        
 
 
 
